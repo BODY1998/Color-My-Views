604243e025760f30f7b453f410ca1ef0871d9591
BODY1998 <aymanabdo815@gmail.com>
2022-10-16 23:16:54 +0200

    last commit

--- app/src/main/java/com/obaida/colormyviews/BoxesNames.kt
+++ app/src/main/java/com/obaida/colormyviews/BoxesNames.kt
@@ -5,5 +5,8 @@ data class BoxesNames(
     var box_two: String = "",
     var box_three: String = "",
     var box_four: String = "",
-    var box_five: String = ""
+    var box_five: String = "",
+    var redButton: String = "",
+    var greenButton: String = "",
+    var yellowButton: String = "",
 )
--- app/src/main/java/com/obaida/colormyviews/MainActivity.kt
+++ app/src/main/java/com/obaida/colormyviews/MainActivity.kt
@@ -23,7 +23,10 @@ class MainActivity : AppCompatActivity() {
             binding.boxTwoText,
             binding.boxThreeText,
             binding.boxFourText,
-            binding.boxFiveText
+            binding.boxFiveText,
+            binding.redButton,
+            binding.yellowButton,
+            binding.greenButton
         )
         for (item in clickableView) {
             item.setOnClickListener { makeColored(it) }
@@ -41,6 +44,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
